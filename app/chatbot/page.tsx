cb4a9794cd7592ebfce868277f552852c22bb695
Preeti Gupta <preetigupta@Preetis-MacBook-Pro.local>
2025-04-05 20:54:48 +0530

    Add login and chatbot pages

---
+++ app/chatbot/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChatbotPage = () => {
+  return (
+    <div>
+      <h1>Chatbot</h1>
+      <p>Chatbot functionality coming soon!</p>
+    </div>
+  );
+};
+
+export default ChatbotPage;
